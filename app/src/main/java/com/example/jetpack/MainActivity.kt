1fb1a09d75675c17161ef58e131c5e1fdcacc6a0
Christine Kim <kkej0630@gmail.com>
2021-10-19 11:45:31 -0400

    add multiple nested composables

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -3,27 +3,54 @@ package com.example.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpack.model.Message
 
-class MainActivity : ComponentActivity() {
+internal class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Xtine's Jetpack App!")
+            MessageCard(Message("xtine", "I'm learning!"))
         }
     }
 
     @Composable
-    fun MessageCard(name: String) {
-        Text("Hey there, $name!")
+    fun MessageCard(message: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.piehole),
+                contentDescription = "Author profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text("${message.author} said:")
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = message.body)
+            }
+        }
     }
 
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard("Player One")
+        MessageCard(Message("Xtine", "I love my bunny."))
     }
 }
---
+++ app/src/main/java/com/example/jetpack/model/Message.kt
@@ -0,0 +1,6 @@
+package com.example.jetpack.model
+
+internal data class Message(
+    val author: String,
+    val body: String
+)
